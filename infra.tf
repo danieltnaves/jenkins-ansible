c3f4a296ee4b971f1d68491a0e5cd95e2866abf6
Daniel <daniel.naves@outlook.com>
2019-04-13 11:16:01 -0300

    Add ports rules to terraform

--- infra.tf
+++ infra.tf
@@ -1,11 +1,47 @@
 provider "aws" {
-  access_key = "--"
-  secret_key = "--"
+  access_key = "AKIAIKSKIQVV7UXMMP5A"
+  secret_key = "vOZgI8AnWeN+rp1QrQsQ1bn+8KfpNU52oY+QSjnF"
   region     = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
+resource "aws_instance" "jenkins-server" {
+  ami           = "ami-0de53d8956e8dcf80"
   instance_type = "t2.micro"
   key_name      = "servidor-estudos"
+  security_groups = [
+        "allow_jenkins"
+    ]
+}
+
+resource "aws_security_group" "allow_jenkins" {
+  name        = "allow_jenkins"
+  description = "Allow Jenkins"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
